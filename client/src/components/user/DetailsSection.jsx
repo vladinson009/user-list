ee4b12ac2d0ae387d48ca03205ebde5ade9cf675
vladinSon <vladinson009@icloud.com>
2024-12-29 03:45:36 +0100

    populate detail section with data

--- client/src/components/user/DetailsSection.jsx
+++ client/src/components/user/DetailsSection.jsx
@@ -1,9 +1,12 @@
 import PropTypes from 'prop-types'
+import { dateReadParser } from '../../utils/dateParser'
 DetailsSection.propTypes = {
     onClose: PropTypes.func.isRequired
 }
-export default function DetailsSection({ onClose }) {
-
+export default function DetailsSection({ onClose, user }) {
+    const address = Object.values(user.address).join(', ')
+    const createdAt = dateReadParser(user.createdAt)
+    const updatedAt = dateReadParser(user.updatedAt)
     return (
         <div className="overlay">
             <div onClick={onClose} className="backdrop"></div>
@@ -26,20 +29,20 @@ export default function DetailsSection({ onClose }) {
                                 className="image" />
                         </div>
                         <div className="user-details">
-                            <p>User Id: <strong>62bb0c0eda039e2fdccba57b</strong></p>
+                            <p>User Id: <strong>{user._id}</strong></p>
                             <p>
                                 Full Name:
-                                <strong> Peter Johnson </strong>
+                                <strong> {user.firstName} </strong>
                             </p>
-                            <p>Email: <strong>peter@abv.bg</strong></p>
-                            <p>Phone Number: <strong>0812345678</strong></p>
+                            <p>Email: <strong>{user.email}</strong></p>
+                            <p>Phone Number: <strong>{user.phoneNumber}</strong></p>
                             <p>
                                 Address:
-                                <strong> Bulgaria, Sofia, Aleksandar Malinov 78 </strong>
+                                <strong> {address} </strong>
                             </p>
 
-                            <p>Created on: <strong>Wednesday, June 28, 2022</strong></p>
-                            <p>Modified on: <strong>Thursday, June 29, 2022</strong></p>
+                            <p>Created on: <strong>{createdAt}</strong></p>
+                            <p>Modified on: <strong>{updatedAt}</strong></p>
                         </div>
                     </div>
                 </div>

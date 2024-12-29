c52c12e2b22c5f8df35965bb21811ac9b6d7ed26
vladinSon <vladinson009@icloud.com>
2024-12-29 03:43:17 +0100

    created delete user api

--- client/src/api/user.js
+++ client/src/api/user.js
@@ -3,14 +3,17 @@ import api from './api';
 function getUsers() {
   return api.get('/users');
 }
-function updateOrCreateUser(data, id, newDate) {
+function updateOrCreateUser(data, newDate, id) {
   if (id) {
     data.updatedAt = newDate;
     return api.put('/users/' + id, data);
   } else {
     data.createdAt = newDate;
+    data.updatedAt = newDate;
     return api.post('/users', data);
   }
 }
-
-export default { getUsers, updateOrCreateUser };
+function deleteUser(id) {
+  return api.del('/users/' + id);
+}
+export default { getUsers, updateOrCreateUser, deleteUser };

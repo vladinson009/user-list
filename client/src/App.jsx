ca711b7c746978f18a7bcc6a57ed8e343d6d91ae
vladinSon <vladinson009@icloud.com>
2024-12-29 22:36:18 +0100

    visual changes, deleted empty rows

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,19 +2,15 @@ import FooterSection from "./components/footer/FooterSection"
 import HeaderSection from "./components/header/HeaderSection"
 import TableSection from "./components/table/TableSection"
 
-
 function App() {
-
   return (
     <>
       <HeaderSection />
       <main className="main">
         <TableSection />
       </main>
-
       <FooterSection />
     </>
   )
 }
-
 export default App

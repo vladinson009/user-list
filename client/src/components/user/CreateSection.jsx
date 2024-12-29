ac131b8432f92d26700a61d3172ec1e546c87079
vladinSon <vladinson009@icloud.com>
2024-12-29 16:38:59 +0100

    removed pagination function from component

--- client/src/components/user/CreateSection.jsx
+++ client/src/components/user/CreateSection.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import userApi from '../../api/user'
 import { dateWriteParser } from '../../utils/dateParser'
-export default function CreateSection({ onClose, user, onType, set, pagination }) {
+export default function CreateSection({ onClose, user, onType, set }) {
     async function onUpdate(e) {
         e.preventDefault()
         const date = dateWriteParser(new Date())

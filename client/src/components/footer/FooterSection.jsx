a4576c0875b1113d925517d7ada7dcdbf0457ea2
vladinSon <vladinson009@icloud.com>
2024-12-29 02:24:41 +0100

    added Footer section

---
+++ client/src/components/footer/FooterSection.jsx
@@ -0,0 +1,8 @@
+export default function FooterSection() {
+
+    return (
+        <footer className="footer">
+            <p>This site is designed to be used for training purposes at SoftUni.</p>
+        </footer>
+    )
+}

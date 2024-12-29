d05fd7d6fc75e39ff0acb0a4181852f6ec247a95
vladinSon <vladinson009@icloud.com>
2024-12-29 02:23:47 +0100

    imported components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,18 @@
+import FooterSection from "./components/footer/FooterSection"
+import HeaderSection from "./components/header/HeaderSection"
+import TableSection from "./components/table/TableSection"
 
 
 function App() {
 
   return (
     <>
+      <HeaderSection />
+      <main className="main">
+        <TableSection />
+      </main>
+
+      <FooterSection />
     </>
   )
 }
